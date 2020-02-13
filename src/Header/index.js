48151052e2a90a355f43c4abd10dd4d8e23f6fa8
f-koki <57610613+f-koki@users.noreply.github.com>
2020-02-13 12:47:52 +0900

    feat: fix the color in nav bar

--- src/Header/index.js
+++ src/Header/index.js
@@ -3,9 +3,10 @@ import { Navbar, Nav } from 'react-bootstrap'
 
 function Header() {
   return (
-    <Navbar className="Header" bg="light" fixed="true">
-      <Nav className="mr-auto">
-        <Nav.Link href="#work">Work</Nav.Link>
+    <Navbar className="Header" bg="dark" variant="dark" fixed="true">
+      <Navbar.Brand>Futsu Tech</Navbar.Brand>
+      <Nav>
+        <Nav.Link href="#work" >Work</Nav.Link>
       </Nav>
     </Navbar>
   );
